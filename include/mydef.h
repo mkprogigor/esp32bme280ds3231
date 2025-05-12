a3ae28ab6c63c2f0a0033263d99e1ab69c5dea60
Igor Mkprog <mkprogigor@gmail.com>
2025-05-12 16:38:57 +0200

    Init first.

---
+++ include/mydef.h
@@ -0,0 +1,10 @@
+// local definitions
+#ifndef MYDEF_H
+#define MYDEF_H
+
+#define WIFI_SSID "Pixel7ik"
+#define WIFI_PASS "88888888"
+#define MYTS_WR_APIKEY "JVJ72ILLDFV0N0X4";
+#define MYTS_RD_APIKEY "I0KTR29NKBSLKH7B";
+
+#endif
